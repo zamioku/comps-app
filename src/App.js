67720220f4dad28057f8ddea366e0d1daa930fab
Sorin Dragomir <sorin.dragomir90@gmail.com>
2024-03-06 04:10:01 +0200

    Added Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import Button from "./Button";
+
+import { GoDatabase, GoCloud, GoBellFill } from "react-icons/go";
+
 function App() {
-    return <div>App</div>
+  return (
+    <div>
+      <div>
+        <Button success rounded outline>
+          <GoBellFill /> Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          <GoCloud /> Buy now!
+        </Button>
+      </div>
+      <div>
+        <Button warning>
+          <GoDatabase /> See deal!
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Something
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
