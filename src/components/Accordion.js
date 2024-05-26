b172542e37374ad4db79f20d63b856746be395a6
Sorin Dragomir <sorin.dragomir90@gmail.com>
2024-05-26 18:49:39 +0300

    Added icons and styling

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,22 +1,41 @@
 import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }) {
-  const [expandedIndex, setExpandedIndex] = useState(0);
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
+  const handleClick = (nextIndex) => {
+    if (expandedIndex === nextIndex) {
+      setExpandedIndex(-1);
+    } else {
+      setExpandedIndex(nextIndex);
+    }
+  };
 
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
-
     // const content = isExpanded && <div>{item.content}</div>; // conditional rendering!
+    const icon = (
+      <span className="text-2xl">
+        {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+      </span>
+    );
 
     return (
       <div key={item.id}>
-        <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
-        {isExpanded && <div>{item.content}</div>}
+        <div
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+          onClick={() => handleClick(index)}
+        >
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
     );
   });
 
-  return <div>{renderedItems}</div>;
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
 }
 
 export default Accordion;
